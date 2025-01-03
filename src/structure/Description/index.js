3150954ca1e64a2e8f5ca63fe761ea9fa88809f0
Dolores Polito <dolores.polito@gmail.com>
2025-01-03 15:07:30 -0300

    ?

--- src/structure/Description/index.js
+++ src/structure/Description/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import SplitWords from "../../components/SplitWords";
-import Treatments from "../../components/Treatments";
+import SplitWords from "@/components/SplitWords";
+import Treatments from "@/components/Treatments";
 import styles from "./styles.module.scss";
 import BookButton from "@/components/BookButton";
 export default function Description() {
