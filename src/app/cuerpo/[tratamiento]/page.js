32f7aa02af0d99652ac79ee0596c592e548dfff7
Dolores Polito <dolores.polito@gmail.com>
2025-01-27 14:44:35 -0300

    fix?

--- src/app/cuerpo/[tratamiento]/page.js
+++ src/app/cuerpo/[tratamiento]/page.js
@@ -1,24 +1,26 @@
 // import Treatment from "../../../components/Treatment";
-import cuerpo from "../../../jsons/cuerpo.json";
+// import cuerpo from "../../../jsons/cuerpo.json";
 import ColorChangeTreatmentsOnScrollGsap from "@/components/ChangeColorTreatments"
 import { AnimatedHomeHeader } from "@/structure/NavbarToia";
 
 export default function TreatmentPage({ params }) {
-  const { tratamiento } = params;
+  // const { tratamiento } = params;
 
-  const data = cuerpo[0].tratamientos.find(
-    (item) => item.id === tratamiento
-  );
+  // const data = cuerpo[0].tratamientos.find(
+  //   (item) => item.id === tratamiento
+  // );
 
   // Si no se encuentra, puedes mostrar un mensaje de error o redirigir
-  if (!data) {
-    return <p style={{ marginTop: "20vh" }}>Tratamiento no encontrado.</p>;
-  }
+  // if (!data) {
+  //   return <p style={{ marginTop: "20vh" }}>Tratamiento no encontrado.</p>;
+  // }
 
   return (
     <>
        <AnimatedHomeHeader />
-      <ColorChangeTreatmentsOnScrollGsap data={data} />
+      <ColorChangeTreatmentsOnScrollGsap
+      //  data={data} 
+       />
     </>
   );
 }
